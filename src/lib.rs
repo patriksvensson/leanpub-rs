0ed81f670c67957624765d2069c1e87d96ec52a7
Patrik Svensson <patrik@patriksvensson.se>
2018-06-28 22:41:26 +0200

    Added documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,17 @@ mod response;
 use errors::LeanpubResult;
 pub use response::Summary;
 
+/// The Leanpub API client.
 pub struct Client {
     slug: String,
     api_key: Option<String>,
 }
 
 impl Client {
+    /// Creates a new client for a specific slug.
+    /// The API key is optional but required when 
+    /// interacting with API endpoints that requires
+    /// an authenticated user.
     pub fn new<T: Into<String>>(slug: T, api_key: Option<T>) -> Self {
         return Client { 
             slug: slug.into(), 
@@ -27,6 +32,15 @@ impl Client {
         };
     }
 
+    /// Gets the books summary information.
+    /// 
+    /// # Note
+    /// 
+    /// This does not require authentication. If you do provide an API key, 
+    /// and you are an author of the book, then we will also give you the 
+    /// total copies sold and revenue for the book, as well as the URLs for 
+    /// downloading the current preview and published version of your book 
+    /// in epub, pdf and mobi formats.
     pub fn get_summary(&self) -> LeanpubResult<Summary> {
         let uri = self.append_api_key(format!("https://leanpub.com/{}.json", self.slug));
         let response = reqwest::get(&uri[..])?.text()?;
--- src/response.rs
+++ src/response.rs
@@ -1,40 +1,65 @@
 use serde::{de, Deserialize, Deserializer};
 
+/// Contains summary information about a book.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Summary {
+    /// The book's ID.
     pub id: u32,
+    /// The book's title.
     pub title: String,
+    /// The book's subtitle.
     #[serde(deserialize_with = "de_empty_str_to_optional_str")]
     pub subtitle: Option<String>,
+    /// The book's slug.
     pub slug: String,
+    /// The book's description.
     #[serde(deserialize_with = "de_empty_str_to_optional_str")]
     pub about_the_book: Option<String>,
+    /// The book's total revenue.
     #[serde(default, deserialize_with = "de_str_to_f32")]
     pub total_revenue: f32,
+    /// The date the book was last published.
     pub last_published_at: Option<String>,
+    /// The number of words.
     #[serde(default)]
     pub word_count: u32,
+    /// The number of pages.
     #[serde(default)]
     pub page_count: u32,
+    /// The number of words in the published book.
     #[serde(default, deserialize_with = "de_nullable_int_to_u32")]
     pub word_count_published: u32,
+    /// The number of pages in the published book.
     #[serde(default, deserialize_with = "de_nullable_int_to_u32")]
     pub page_count_published: u32,
+    /// The number of total sold copies.
     #[serde(default)]
     pub total_copies_sold: u32,
+    /// The meta description of the book.
     pub meta_description: Option<String>,
+    /// Contains author information.
     pub author_string: String,
+    /// The book's URL.
     pub url: String,
+    /// A large image of the book's title page.
     pub title_page_url: String,
     #[serde(deserialize_with = "de_str_to_f32")]
+    /// The book's minimum price.
     pub minimum_price: f32,
     #[serde(deserialize_with = "de_str_to_f32")]
+    /// The book's suggested price.
     pub suggested_price: f32,
+    /// A small image of the book's title page.
     pub image: String,
     #[serde(default)]
+    /// The number of user's who has an interest in
+    /// purchasing the book.
     pub possible_reader_count: u32,
+    /// The URL to the PDF preview.
     pub pdf_preview_url: Option<String>,
+    /// The URL to the epub preview.
     pub epub_preview_url: Option<String>,
+    /// The URL to the mobi preview.
     pub mobi_preview_url: Option<String>,
 }
 
